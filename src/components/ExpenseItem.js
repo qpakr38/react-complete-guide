a63cc0a5ba3a3ea38e9803be4a5aa24e8c2a46d1
qpakr38 <zczc0819@gmail.com>
2022-08-28 23:19:21 +0900

    FEAT : {}을 이용하여 동적 변수 적용하기

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 8, 29);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
     return (
         <div className="expense-item">
-            <div>Date</div>
+            <div>{expenseDate.toDateString()}</div>
             <div className="expense-item__description">
-                <h2>Title</h2>
-                <div className="expense-item__price">Amount</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     );
