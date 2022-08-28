ea925bbb58de3fb1a78e5df236d602b61026b01c
qpakr38 <zczc0819@gmail.com>
2022-08-28 22:53:12 +0900

    FEAT : 사용자 컴포넌트 요구사항 변경
    Date, Title, Amount 변경

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,13 @@
-function ExpenseItem(){
-    return(
+function ExpenseItem() {
+    return (
         <div>
-            <h2>Expense Item!</h2>
+            <div>Date</div>
+            <div>
+                <h2>Title</h2>
+                <div>Amount</div>
+            </div>
         </div>
-    )
+    );
 }
+
 export default ExpenseItem;
