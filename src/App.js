29628b7c4cfe903cc821850414a62c026bf70feb
qpakr38 <zczc0819@gmail.com>
2022-08-28 23:36:00 +0900

    FEAT : props를 활용한 컴포넌트 재활용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,69 @@
-import ExpenseItem  from "./components/ExpenseItem";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
-    </div>
-  );
+    const expenses = [
+        {
+            id: 'e1',
+            title: "Car Insurance",
+            amount: 194.67,
+            date: new Date(1111, 1, 1)
+        },
+        {
+            id: 'e2',
+            title: "Car Insurance2",
+            amount: 294.67,
+            date: new Date(2222, 2, 2)
+        },
+        {
+            id: 'e3',
+            title: "Car Insurance3",
+            amount: 394.67,
+            date: new Date(3333, 3, 3)
+        },
+        {
+            id: 'e4',
+            title: "Car Insurance4",
+            amount: 494.67,
+            date: new Date(4444, 4, 4)
+        },
+        {
+            id: 'e5',
+            title: "Car Insurance5",
+            amount: 594.67,
+            date: new Date(5555, 5, 5)
+        }
+    ];
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            <ExpenseItem
+            title = {expenses[0].title}
+            amount = {expenses[0].amount}
+            date = {expenses[0].date}
+            ></ExpenseItem>
+            <ExpenseItem
+            title = {expenses[1].title}
+            amount = {expenses[1].amount}
+            date = {expenses[1].date}
+            ></ExpenseItem>
+            <ExpenseItem
+            title = {expenses[2].title}
+            amount = {expenses[2].amount}
+            date = {expenses[2].date}
+            ></ExpenseItem>
+            <ExpenseItem
+            title = {expenses[3].title}
+            amount = {expenses[3].amount}
+            date = {expenses[3].date}
+            ></ExpenseItem>
+            <ExpenseItem
+            title = {expenses[4].title}
+            amount = {expenses[4].amount}
+            date = {expenses[4].date}
+            ></ExpenseItem>
+            }
+        </div>
+    );
 }
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    const expenseDate = new Date(2022, 8, 29);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <div>{expenseDate.toDateString()}</div>
+            <div>{props.date.toDateString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
