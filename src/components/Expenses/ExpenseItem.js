4ded9104ee973bf934336463f36229fb773c3d52
qpakr38 <zczc0819@gmail.com>
2022-08-30 21:43:43 +0900

    REFACTOR : JSX 코드를 단순화하기 위해 함수로 선언하여 사용

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,10 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+    const clickHandler = () => {
+
+        console.log('Clicked!')
+    }
     return (
         <Card className="expense-item">
             <div>
@@ -12,7 +16,8 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={()=>{console.log('Clicked!')}}>Change Tile</button>
+            <button onClick={clickHandler}>Change Tile
+            </button>
         </Card>
     );
 }
