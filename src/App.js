f9aa18747166ac8651a72c6d4ed1cf74a929e076
qpakr38 <zczc0819@gmail.com>
2022-08-31 19:29:00 +0900

    FEAT : Filter 기능을 통한 조건부 렌더링 기능 구현

--- src/App.js
+++ src/App.js
@@ -8,31 +8,31 @@ const DUMMY_EXPENSES = [
         id: 'e1',
         title: "Car Insurance",
         amount: 194.67,
-        date: new Date(1111, 1, 1)
+        date: new Date(2022, 1, 1)
     },
     {
         id: 'e2',
         title: "Car Insurance2",
         amount: 294.67,
-        date: new Date(2222, 2, 2)
+        date: new Date(2022, 2, 2)
     },
     {
         id: 'e3',
         title: "Car Insurance3",
         amount: 394.67,
-        date: new Date(3333, 3, 3)
+        date: new Date(2022, 3, 3)
     },
     {
         id: 'e4',
         title: "Car Insurance4",
         amount: 494.67,
-        date: new Date(4444, 4, 4)
+        date: new Date(2021, 4, 4)
     },
     {
         id: 'e5',
         title: "Car Insurance5",
         amount: 594.67,
-        date: new Date(5555, 5, 5)
+        date: new Date(2021 , 5, 5)
     }
 ];
 const App = () => {
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,14 +8,20 @@ import ExpensesFilter from "../NewExpense/ExpensesFilter";
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2022');
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear
+    });
+
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear)
     };
+
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-            {props.items.map((expense) => (
+            {filteredExpenses.map((expense) => (
                 <ExpenseItem
+                    key={expense.id}
                     title={expense.title}
                     amount={expense.amount}
                     date={expense.date}
