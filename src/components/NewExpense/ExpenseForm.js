4ab2cc4f80e49572f237942796e8b18544c49cad
qpakr38 <zczc0819@gmail.com>
2022-08-31 13:41:14 +0900

    FEAT : 하나의 state에 여러개의 state 관리하기
    ...userInput을 활용하여 기존 state을 보존할수 있다.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,18 +3,35 @@ import React, {useState} from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+    //const [enteredTitle, setEnteredTitle] = useState('');
+    //const [enteredAmount, setEnteredAmount] = useState('');
+    //const [enteredDate, setEnteredDate] = useState('');
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    });
 
     const titleChangHandler = (event) => {
-        setEnteredTitle(event.target.value);
+        //setEnteredTitle(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredTitle: event.target.value
+        });
     }
     const amountChangHandler = (event) => {
-        setEnteredAmount(event.target.value);
+        //setEnteredAmount(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.target.value
+        });
     }
     const dateChangHandler = (event) => {
-        setEnteredDate(event.target.value);
+        //setEnteredDate(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value
+        });
     }
     return (
         <form>
