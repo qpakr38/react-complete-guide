5c212ce76c55b86445218542290da9b28460251a
qpakr38 <zczc0819@gmail.com>
2022-08-31 18:42:07 +0900

    FEAT : ExpensesFilter 컴포넌트 생성

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,19 @@
+import React, {useState} from "react";
 import "./Expenses.css";
+
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "../NewExpense/ExpensesFilter";
+
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2022');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear)
+    };
     return (
         <Card className="expenses">
+            <ExpensesFilter selected = {filteredYear} onChangeFilter={filterChangeHandler}/>
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
