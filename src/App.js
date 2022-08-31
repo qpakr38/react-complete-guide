fe9ae645fe0f3f7d29a714177bd8ce4d79e4a1c7
qpakr38 <zczc0819@gmail.com>
2022-08-31 18:55:52 +0900

    FEAT : State를 활용한 동적 렌더링 구현

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
 import Expenses from "./components/Expenses/Expenses";
-//import React from "react";
+import React, {useState} from "react";
 
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        title: "Car Insurance",
+        amount: 194.67,
+        date: new Date(1111, 1, 1)
+    },
+    {
+        id: 'e2',
+        title: "Car Insurance2",
+        amount: 294.67,
+        date: new Date(2222, 2, 2)
+    },
+    {
+        id: 'e3',
+        title: "Car Insurance3",
+        amount: 394.67,
+        date: new Date(3333, 3, 3)
+    },
+    {
+        id: 'e4',
+        title: "Car Insurance4",
+        amount: 494.67,
+        date: new Date(4444, 4, 4)
+    },
+    {
+        id: 'e5',
+        title: "Car Insurance5",
+        amount: 594.67,
+        date: new Date(5555, 5, 5)
+    }
+];
 const App = () => {
-    const expenses = [
-        {
-            id: 'e1',
-            title: "Car Insurance",
-            amount: 194.67,
-            date: new Date(1111, 1, 1)
-        },
-        {
-            id: 'e2',
-            title: "Car Insurance2",
-            amount: 294.67,
-            date: new Date(2222, 2, 2)
-        },
-        {
-            id: 'e3',
-            title: "Car Insurance3",
-            amount: 394.67,
-            date: new Date(3333, 3, 3)
-        },
-        {
-            id: 'e4',
-            title: "Car Insurance4",
-            amount: 494.67,
-            date: new Date(4444, 4, 4)
-        },
-        {
-            id: 'e5',
-            title: "Car Insurance5",
-            amount: 594.67,
-            date: new Date(5555, 5, 5)
-        }
-    ];
-
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
     const addExpenseHandler = expense => {
-        console.log("In App.js");
-        console.log(expense);
-        console.log(expenses);
+        setExpenses(prevExpense =>{
+            return [expense,...prevExpense];
+        });
     };
     // return React.createElement(
     //     'div',
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,32 +13,14 @@ const Expenses = (props) => {
     };
     return (
         <Card className="expenses">
-            <ExpensesFilter selected = {filteredYear} onChangeFilter={filterChangeHandler}/>
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}
-            />
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}
-            />
-            <ExpenseItem
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date}
-            />
-            <ExpenseItem
-                title={props.items[3].title}
-                amount={props.items[3].amount}
-                date={props.items[3].date}
-            />
-            <ExpenseItem
-                title={props.items[4].title}
-                amount={props.items[4].amount}
-                date={props.items[4].date}
-            />
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+            {props.items.map((expense) => (
+                <ExpenseItem
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                />)
+            )}
         </Card>
 
     );
