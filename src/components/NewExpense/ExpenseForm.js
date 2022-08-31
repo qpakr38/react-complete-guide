8a1dd229a604a8db82654cc20507e8de91233fb0
qpakr38 <zczc0819@gmail.com>
2022-08-31 13:57:14 +0900

    FEAT : value와 state를 활용한 양방향 바인딩

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -51,21 +51,35 @@ const ExpenseForm = () => {
             date: new Date(enteredDate)
         };
         console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     }
     return (
         <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type='text' onChange={titleChangHandler}/>
+                    <input
+                        type='text'
+                        value={enteredTitle}
+                        onChange={titleChangHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step='0.01' onChange={amountChangHandler}/>
+                    <input
+                        type="number"
+                        min="0.01" step='0.01'
+                        value={enteredAmount}
+                        onChange={amountChangHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2022-01-01" max='2022-12-31' onChange={dateChangHandler}/>
+                    <input
+                        type="date"
+                        min="2022-01-01" max='2022-12-31'
+                        value={enteredDate}
+                        onChange={dateChangHandler}/>
                 </div>
             </div>
             <div className='new-expense__actions'>
