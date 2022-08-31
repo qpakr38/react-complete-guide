46f568946a753906b1bea16463bc1e584a1a92b7
qpakr38 <zczc0819@gmail.com>
2022-08-31 11:29:18 +0900

    FEAT : State를 활용한 타이틀 변경 기능 구현

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,15 @@
+import React, {useState} from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-    const clickHandler = () => {
+    const [title, setTitle] = useState(props.title);
+;
 
-        console.log('Clicked!')
+    const clickHandler = () => {
+        setTitle('Updated!');
     }
     return (
         <Card className="expense-item">
@@ -13,7 +17,7 @@ const ExpenseItem = (props) => {
                 <ExpenseDate date={props.date}/>
             </div>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Tile
