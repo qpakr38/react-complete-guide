f01ce0ef8d21ec494459f6fc70bef1ac75c39227
qpakr38 <zczc0819@gmail.com>
2022-08-31 19:43:47 +0900

    REFACTORY : JSX에서 조건문 분리하여 구조를 개선함.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,21 +15,22 @@ const Expenses = (props) => {
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear)
     };
-
+    let expensesContent = <p>No expenses found.</p>;
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        ));
+    }
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-            {filteredExpenses.length === 0 && (<p>No expenses found.</p>)}
-            {filteredExpenses.length > 0 && filteredExpenses.map((expense) => (
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />)
-            )}
+            {expensesContent}
         </Card>
-
     );
 }
 
