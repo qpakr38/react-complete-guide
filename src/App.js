658fc5234b72a1427b848c7f007941da9dc7d7d5
qpakr38 <zczc0819@gmail.com>
2022-08-31 13:24:37 +0900

    FEAT : onChange EventHandler 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Expenses from "./components/Expenses/Expenses";
 //import React from "react";
 
+import NewExpense from "./components/NewExpense/NewExpense";
+
 const App = () => {
     const expenses = [
         {
@@ -42,8 +44,8 @@ const App = () => {
     // );
     return (
         <div>
-            <h2>Let's get started!</h2>
-            <Expenses items= {expenses}/>
+            <NewExpense/>
+            <Expenses items={expenses}/>
         </div>
     );
 }
